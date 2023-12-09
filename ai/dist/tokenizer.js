4a64fd7c5339421b91c7e933ff1956ae4ddacf3c
Wolfgang Gassler <wolfgang@gassler.org>
2023-12-09 23:16:19 +0100

    dist comitted by accident

--- ai/dist/tokenizer.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.guessNumberOfTokens = void 0;
-const guessNumberOfTokens = (text) => {
-    // based on https://help.openai.com/en/articles/4936856-what-are-tokens-and-how-to-count-them
-    // 1 token = 4 characters, 100 tokens = 75 words
-    return text.split(" ").length * 1.33;
-};
-exports.guessNumberOfTokens = guessNumberOfTokens;
